cdb421d81f32d61ca32cb5da504f4add41790d5e
gavellaneda <avellaneda.gabriel@gmail.com>
2015-03-06 21:37:42 +0000

    Added the HttpClient plugin full working version.

--- framework.lua
+++ framework.lua
@@ -129,4 +129,64 @@ framework.NetPlugin = NetPlugin
 local HttpPlugin = Plugin:extend()
 framework.HttpPlugin = HttpPlugin
 
+function HttpPlugin:initialize(params)
+	Plugin.initialize(self, params)
+	
+	self.reqOptions = {
+		host = params.host,
+		port = params.port,
+		path = params.path
+	}
+end
+
+function HttpPlugin:error(err)
+	local msg = tostring(err)
+
+	print(msg)
+end
+
+function HttpPlugin:makeRequest(reqOptions, successCallback)
+	local req = http.request(reqOptions, function (res)
+
+
+		local data = ''
+		
+		res:on('data', function (chunk)
+			data = data .. chunk	
+			successCallback(data)
+			-- TODO: Verify when data its complete or when we need to use de end
+		end)
+
+		res:on('error', function (err)
+			local msg = 'Error while receiving a responde: ' .. err
+			self.error(msg)
+		end)
+
+	end)
+	
+	req:on('error', function (err)
+		local msg = 'Error while sending a request: ' .. err
+		self.error(msg)
+	end)
+
+	req:done()
+end
+
+function HttpPlugin:onPoll()
+	self:makeRequest(self.reqOptions, function (data)
+		self:parseResponse(data)
+	end)
+end
+
+function HttpPlugin:parseResponse(data)
+	local metrics = self:onParseResponse(data) 
+	self:report(metrics)
+end
+
+function HttpPlugin:onParseResponse(data)
+	-- To be overriden on class instance
+	print(data)
+	return {}
+end
+
 return framework
