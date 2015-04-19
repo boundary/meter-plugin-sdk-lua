5bd4185f01e08ed11d884f735af357006a2e508f
gavellaneda <avellaneda.gabriel@gmail.com>
2015-04-18 23:32:47 -0300

    Added PollerCollection, WebRequestDataSource

--- framework.lua
+++ framework.lua
@@ -611,6 +611,83 @@ function DataSourcePoller:run(callback)
   self:_poll(callback)
 end
 
+local PollerCollection = Emitter:extend()
+function PollerCollection:initialize(pollers) 
+  self.pollers = pollers or {}
+
+end
+
+function PollerCollection:add(poller)
+  table.insert(self.pollers, poller)
+end
+
+function PollerCollection:run(callback) 
+  if self.running then
+    return
+  end
+
+  self.running = true
+  for _,p in pairs(self.pollers) do
+    p:run(callback)
+  end
+
+end
+
+local WebRequestDataSource = DataSource:extend()
+function WebRequestDataSource:initialize(params)
+	local options = params
+	if type(params) == 'string' then
+		options = url.parse(params)
+	end
+
+  self.wait_for_end = options.wait_for_end or false
+
+	self.options = options
+  self.info = options.meta
+end
+
+function WebRequestDataSource:request(reqOptions, callback)
+  return http.get(reqOptions, callback)
+end
+
+function WebRequestDataSource:fetch(context, callback)
+  assert(callback, 'WebRequestDataSource:fetch: callback is required')
+
+	local headers = nil 
+	local buffer = ''
+	local reqOptions = {
+		host = self.options.host,
+		port = self.options.port,
+		path = self.options.path,
+		headers = headers
+	}
+
+	local success = function (res) 
+
+    if self.wait_for_end then
+		  res:on('end', function ()
+        callback(buffer, self.info)
+      end)
+    else 
+      res:once('data', function (data)
+        if not self.wait_for_end then
+          callback(buffer, self.info)
+        end
+      end)
+    end 
+
+		res:on('data', function (data) 
+			buffer = buffer .. data
+    end)
+
+    res:propagate('error', self)
+	end
+
+	local req = self:request(reqOptions, success)
+	req:propagate('error', self)
+end
+
+
 --- RandomDataSource class
 -- @type RandomDataSource
 local RandomDataSource = DataSource:extend()
@@ -639,5 +716,9 @@ end
 
 framework.RandomDataSource = RandomDataSource
 framework.DataSourcePoller = DataSourcePoller
+framework.WebRequestDataSource = WebRequestDataSource
+framework.PollerCollection = PollerCollection
 
 return framework
+
+
